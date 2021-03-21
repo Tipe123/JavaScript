ddba61b43c7969d2fba13d0c9fdf36c530a7127b
Tipe123 <monyemangene.2@gmail.com>
2021-03-21 22:03:44 +0200

    End of For-in-loops

--- for-in-loops.js
+++ for-in-loops.js
@@ -14,3 +14,15 @@ console.log(countries)
 	for(let country in countries ){
 			console.log("The country of " + country + " is in "+countries[country])
 	}
+
+
+
+//applying for in loops in an array
+
+names = ["Tipe", "Timothy", "Rivaldo"];
+
+for(let name in names){
+	console.log(names[name])
+}
+
+//name becomes the index
