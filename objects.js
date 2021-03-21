8fb012dcf44b478e01f9f5656cb40597690de97f
Tipe123 <monyemangene.2@gmail.com>
2021-03-21 21:47:18 +0200

    Done with Objects

--- objects.js
+++ objects.js
@@ -1,11 +1,36 @@
 var dog={
 	name:"Woody",
-	type:"dog"
+	type:"Dog"
 };
 
 var cat={
 	name:"Bob",
-	type:"cat"
+	type:"Cat"
 };
 
-console.log(dog)
+
+//creating an array with a list of objects
+
+var pets = [dog  , cat];
+
+console.log(pets)
+
+//add to the array
+
+var dog2 = {
+	name:"Spider",
+	type:"Bull Dog"
+}
+
+pets.push( dog2)
+console.log("\nAfter Pushing An Element into The Array\n")
+console.log(pets)
+
+//adding a new property into object cat
+
+cat.age = 2;
+
+dog['age'] = 12;
+
+console.log("\nAfter  adding properties in the objects\n")
+console.log(pets)
